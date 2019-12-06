18ed595e35abc36fbc89001b29d6d6073c044387
Dulat <dulat.issenzhulov@inbox.ru>
2019-12-07 04:02:50 +0600

    init

---
+++ precache-manifest.57f231910a573fbead7cff328bf6fb8d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "097b9d7a043b9dd03d2f2266d9b15bcd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d6cbd103d7c38c8ba854",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "70c78b0e816a18c8657a",
+    "url": "/static/js/2.1f07b838.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.1f07b838.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d6cbd103d7c38c8ba854",
+    "url": "/static/js/main.ebac9694.chunk.js"
+  },
+  {
+    "revision": "03357149483a376b5b2e",
+    "url": "/static/js/runtime-main.b09effc8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
